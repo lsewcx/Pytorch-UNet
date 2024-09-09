5bc7966782ea0815b02c146e8ef131ad27cd5e4e
lsewcx <lse2100852163.com>
2024-09-09 17:06:33 +0800

    修改输出

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -3,69 +3,7 @@
 from .unet_parts import *
 
 
-class SKConv(nn.Module):
-    def __init__(self, features, WH, M, G, r, stride=1, L=32):
-        """ Constructor
-        Args:
-            features: 输入通道维度
-            WH: 输入特征图的空间维度
-            M: 分支的数量
-            G: 卷积组的数量
-            r: 计算d，向量s的压缩倍数，C/r
-            stride: 步长，默认为1
-            L: 矢量z的最小维度，默认为32
-        """
-        super(SKConv, self).__init__()
-        d = max(int(features / r), L)
-        self.M = M
-        self.features = features
-        self.convs = nn.ModuleList([])
-        # 使用不同kernel size的卷积，增加不同的感受野
-        for i in range(M):
-            self.convs.append(nn.Sequential(
-                nn.Conv2d(features, features, kernel_size=3 + i * 2, stride=stride, padding=1 + i, groups=G),
-                nn.BatchNorm2d(features),
-                nn.ReLU(inplace=False)
-            ))
-        # 全局平均池化
-        self.gap = nn.AvgPool2d(int(WH / stride))
-        self.fc = nn.Linear(features, d)
-        self.fcs = nn.ModuleList([])
-        # 全连接层
-        for i in range(M):
-            self.fcs.append(
-                nn.Linear(d, features)
-            )
-        self.softmax = nn.Softmax(dim=1)
- 
-    def forward(self, x):
-        ''' Split操作'''
-        for i, conv in enumerate(self.convs):
-            fea = conv(x).unsqueeze_(dim=1)
-            if i == 0:
-                feas = fea
-            else:
-                feas = torch.cat([feas, fea], dim=1)
- 
-        ''' Fuse操作'''
-        fea_U = torch.sum(feas, dim=1)
-        fea_s = self.gap(fea_U).squeeze_()
-        fea_z = self.fc(fea_s)
- 
-        ''' Select操作'''
-        for i, fc in enumerate(self.fcs):
-            # fc-->d*c维
-            vector = fc(fea_z).unsqueeze_(dim=1)
-            if i == 0:
-                attention_vectors = vector
-            else:
-                attention_vectors = torch.cat([attention_vectors, vector], dim=1)
-        # 计算attention权重
-        attention_vectors = self.softmax(attention_vectors)
-        attention_vectors = attention_vectors.unsqueeze(-1).unsqueeze(-1)
-        # 最后一步，各特征图与对应的注意力权重相乘，得到输出特征图V
-        fea_v = (feas * attention_vectors).sum(dim=1)
-        return fea_v
+
 
 
 class UNet_Attention(nn.Module):
@@ -86,7 +24,6 @@ class UNet_Attention(nn.Module):
         self.up3 = (Up(256, 128 // factor, bilinear))
         self.up4 = (Up(128, 64, bilinear))
         self.outc = (OutConv(64, n_classes))
-        self.sk = SKConv(64, 480, 2, 1, 2)
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -98,6 +35,5 @@ class UNet_Attention(nn.Module):
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
-        x = self.sk(x)
         logits = self.outc(x)
-        return logits
+        return  logits
