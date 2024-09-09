7ba5525f02f96a5aec2992f670f63f8120800aa8
lsewcx <lse2100852163.com>
2024-09-09 16:18:31 +0800

    修改输出

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -87,4 +87,4 @@ class UNet_Attention(nn.Module):
         x = self.up4(x, x1)
         x = self.CBAM(x)
         logits = self.outc(x)
-        return nn.Sigmoid()(logits)
+        return torch.sigmoid(logits)
