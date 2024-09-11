481776d86918ac7faba49739588f26600ad904a1
lsewcx <lse210085@163.com>
2024-09-11 19:24:23 +0800

    优化unet++

--- unet/unet_plus_plus.py
+++ unet/unet_plus_plus.py
@@ -45,7 +45,7 @@ class UNetPlusPlus(nn.Module):
         self.is_ds = is_ds
         self.n_channels = n_channels
         self.n_classes = n_classes
-        channels = [32, 64, 128, 256]
+        channels = [16,32, 64, 128, 256]
         
         self.conv0_0 = DoubleConv(n_channels, channels[0])
         self.conv1_0 = DoubleConv(channels[0], channels[1])
