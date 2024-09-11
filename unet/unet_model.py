5479ff730fe6640ce7ad4745708c3af78b8a709a
lsewcx <lse210085@163.com>
2024-09-11 21:49:30 +0800

    abc

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -36,9 +36,9 @@ class UNet_less(nn.Module):
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-        x5 = self.dropout(x5)  # 在下采样的最后一层添加 Dropout
+        # x5 = self.dropout(x5)  # 在下采样的最后一层添加 Dropout
         x = self.up1(x5, x4)
-        x = self.dropout(x)  # 在上采样层之间添加 Dropout
+        # x = self.dropout(x)  # 在上采样层之间添加 Dropout
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
