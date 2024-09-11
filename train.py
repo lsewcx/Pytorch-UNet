79c6928df47840f225395d0aae63ce217459dfe1
lsewcx <lse2100852163.com>
2024-09-11 18:01:40 +0800

    添加训练jupyter

--- train.py
+++ train.py
@@ -158,7 +158,8 @@ def train_model(
                             logger.info(f'\nNew best model with Dice score: {val_score}')
                             Path(dir_checkpoint).mkdir(parents=True, exist_ok=True)
                             torch.save(model, 'best_model.pth')
-                            torch.jit.trace(model, images)
+                            torch.jit.script(model)
+                            torch.jit.save(model, 'best_model.pt')
                             logger.info('Best model saved!')
                         try:
                             experiment.log({
