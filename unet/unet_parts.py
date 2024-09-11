9f216d07a3faa79c62707a6587827d3aee94f43b
lsewcx <lse210085@163.com>
2024-09-11 20:57:06 +0800

    修改激活函数

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -82,7 +82,7 @@ class DoubleConvInceptionResNetV2(nn.Module):
         return self.double_conv(x)
 
 class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
+    """(convolution => [BN] => LeakyReLU) * 2"""
 
     def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
@@ -91,10 +91,10 @@ class DoubleConv(nn.Module):
             self.double_conv = nn.Sequential(
                 nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
                 nn.BatchNorm2d(mid_channels),
-                nn.ReLU(inplace=True),
+                nn.LeakyReLU(inplace=True),
                 nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
                 nn.BatchNorm2d(out_channels),
-                nn.ReLU(inplace=True),
+                nn.LeakyReLU(inplace=True),
             )
 
     def forward(self, x):
