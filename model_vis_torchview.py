a116a267123a362496fa63172d673d8105c472ff
lsewcx <lse210085@163.com>
2024-09-13 14:23:46 +0800

    画出模型的结构

--- model_vis_torchview.py
+++ model_vis_torchview.py
@@ -9,7 +9,7 @@ model = UNetInception(n_channels=3, n_classes=4, bilinear=False)
 x = torch.randn(1, 3, 224, 224)  # 假设输入图像大小为 256x256，通道数为 3
 
 # 生成计算图
-graph = draw_graph(model, input_size=(1, 3, 244, 244))
+graph = draw_graph(model, input_size=(1, 3, 224, 224))
 
 # 保存计算图为 PDF 文件
 graph.visual_graph.render('unet_inception_model_torchview', format='pdf')
