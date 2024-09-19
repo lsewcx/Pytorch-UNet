216aed14eb511c822b86a1b2e7cdbc5b0821f64e
lsewcx <lse210085@163.com>
2024-09-19 18:01:52 +0800

    为提交做准备

--- unet/model.py
+++ unet/model.py
@@ -154,51 +154,42 @@ import torch.nn.functional as F
 #         x = self.up4(x, x1)
 #         logits = self.outc(x)
 #         return logits
-class ASPP(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(ASPP, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=6, dilation=6)
-        self.conv3 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=12, dilation=12)
-        self.conv4 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=18, dilation=18)
-        self.pool = nn.AdaptiveAvgPool2d(1)
-        self.conv5 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        self.final_conv = nn.Conv2d(out_channels * 5, out_channels, kernel_size=1)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DepthwiseSeparableConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
+        super(DepthwiseSeparableConv, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, padding=padding, groups=in_channels, bias=False)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        x1 = self.conv1(x)
-        x2 = self.conv2(x)
-        x3 = self.conv3(x)
-        x4 = self.conv4(x)
-        x5 = self.pool(x)
-        x5 = self.conv5(x5)
-        x5 = F.interpolate(x5, size=x4.size()[2:], mode='bilinear', align_corners=True)
-        x = torch.cat([x1, x2, x3, x4, x5], dim=1)
-        x = self.final_conv(x)
-        return x
-class FPN(nn.Module):
-    def __init__(self, in_channels_list, out_channels):
-        super(FPN, self).__init__()
-        self.lateral_convs = nn.ModuleList()
-        self.fpn_convs = nn.ModuleList()
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        x = self.bn(x)
+        return self.relu(x)
 
-        for in_channels in in_channels_list:
-            self.lateral_convs.append(nn.Conv2d(in_channels, out_channels, kernel_size=1))
-            self.fpn_convs.append(nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1))
+class SEBlock(nn.Module):
+    def __init__(self, in_channels, reduction=16):
+        super(SEBlock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(in_channels, in_channels // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_channels // reduction, in_channels, bias=False), 
+            nn.Sigmoid()
+        )
 
-    def forward(self, inputs):
-        # Build laterals
-        laterals = [lateral_conv(inputs[i]) for i, lateral_conv in enumerate(self.lateral_convs)]
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
 
-        # Build top-down path
-        for i in range(len(laterals) - 1, 0, -1):
-            laterals[i - 1] += F.interpolate(laterals[i], size=laterals[i - 1].size()[2:], mode='bilinear', align_corners=True)
-
-        # Build outputs
-        fpn_outs = [self.fpn_convs[i](laterals[i]) for i in range(len(laterals))]
-
-        return fpn_outs
-    
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
@@ -206,18 +197,14 @@ class DoubleConv(nn.Module):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
-            self.double_conv = nn.Sequential(
-                nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-                nn.BatchNorm2d(mid_channels),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU(inplace=True),
-            )
+        self.double_conv = nn.Sequential(
+            DepthwiseSeparableConv(in_channels, mid_channels),
+            DepthwiseSeparableConv(mid_channels, out_channels),
+            SEBlock(out_channels)  # 添加SE模块
+        )
 
     def forward(self, x):
         return self.double_conv(x)
-
 
 class Down(nn.Module):
     """Downscaling with maxpool then double conv"""
@@ -231,7 +218,6 @@ class Down(nn.Module):
 
     def forward(self, x):
         return self.maxpool_conv(x)
-
 
 class Up(nn.Module):
     """Upscaling then double conv"""
@@ -255,12 +241,8 @@ class Up(nn.Module):
 
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
                         diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
-
 
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -270,45 +252,63 @@ class OutConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-class self_net(nn.Module):
+class PyramidPoolingModule(nn.Module):
+    def __init__(self, in_channels, pool_sizes):
+        super(PyramidPoolingModule, self).__init__()
+        self.stages = nn.ModuleList([self._make_stage(in_channels, size) for size in pool_sizes])
+        self.bottleneck = nn.Conv2d(in_channels * (len(pool_sizes) + 1), in_channels, kernel_size=1)
+        self.relu = nn.ReLU(inplace=True)
+
+    def _make_stage(self, in_channels, size):
+        prior = nn.AdaptiveAvgPool2d(output_size=size)
+        conv = nn.Conv2d(in_channels, in_channels, kernel_size=1, bias=False)
+        return nn.Sequential(prior, conv)
+
+    def forward(self, x):
+        h, w = x.size(2), x.size(3)
+        pyramids = [x]
+        for stage in self.stages:
+            pyramids.append(F.interpolate(stage(x), size=(h, w), mode='bilinear', align_corners=True))
+        output = torch.cat(pyramids, dim=1)
+        output = self.bottleneck(output)
+        return self.relu(output)
+
+class self_unet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=False):
-        super(self_net, self).__init__()
+        super(self_unet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
+        self.inc = DoubleConv(n_channels, 32)  # 原来是64
+        self.down1 = Down(32, 64)  # 原来是64, 128
+        self.down2 = Down(64, 128)  # 原来是128, 256
+        self.down3 = Down(128, 256)  # 原来是256, 512
         factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-
-        self.aspp = ASPP(1024 // factor, 256)
-
-        self.fpn = FPN([64, 128, 256, 512, 256], 256)
-
-        self.up1 = Up(256, 256 // factor, bilinear)  # 修复输入通道数
-        self.up2 = Up(512, 128 // factor, bilinear)
-        self.up3 = Up(256, 64 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+        self.down4 = Down(256, 512 // factor)  # 原来是512, 1024 // factor
+        self.ppm = PyramidPoolingModule(512 // factor, [1, 2, 3, 6])  # 添加金字塔池化模块
+        self.up1 = Up(512, 256 // factor, bilinear)  # 原来是1024, 512 // factor
+        self.up2 = Up(256, 128 // factor, bilinear)  # 原来是512, 256 // factor
+        self.up3 = Up(128, 64 // factor, bilinear)  # 原来是256, 128 // factor
+        self.up4 = Up(64, 32, bilinear)  # 原来是128, 64
+        self.outc = OutConv(32, n_classes)  # 原来是64
 
     def forward(self, x):
+        # 下采样部分
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-
-        x5 = self.aspp(x5)
-
-        fpn_outs = self.fpn([x1, x2, x3, x4, x5])
-
-        x = self.up1(fpn_outs[4], fpn_outs[3])
-        x = self.up2(x, fpn_outs[2])
-        x = self.up3(x, fpn_outs[1])
-        x = self.up4(x, fpn_outs[0])
+        
+        # 金字塔池化
+        x5 = self.ppm(x5)
+        
+        # 上采样部分
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
         logits = self.outc(x)
         return logits
 
