473e78313d9ff697e2a13bf5a60a6686ce3f4120
lsewcx <lse210085@163.com>
2024-09-28 22:16:56 +0800

    使用timm库

--- model.py
+++ model.py
@@ -2,19 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class DepthwiseSeparableConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
-        super(DepthwiseSeparableConv, self).__init__()
-        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, padding=padding, groups=in_channels, bias=False)
-        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-    def forward(self, x):
-        x = self.depthwise(x)
-        x = self.pointwise(x)
-        x = self.bn(x)
-        return self.relu(x)
-
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
@@ -23,8 +10,12 @@ class DoubleConv(nn.Module):
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
-            DepthwiseSeparableConv(in_channels, mid_channels),
-            DepthwiseSeparableConv(mid_channels, out_channels),
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
@@ -76,6 +67,47 @@ class OutConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
+# class self_net(nn.Module):
+#     def __init__(self, n_channels=3, n_classes=4):
+#         super(self_net, self).__init__()
+#         self.n_channels = n_channels
+#         self.n_classes = n_classes
+#         self.bilinear = False
+
+#         self.inc = DoubleConv(n_channels, 64)  # 原来是64
+#         self.down1 = Down(64, 128)  # 原来是64, 128
+#         self.down2 = Down(128,256)  # 原来是128, 256
+#         self.down3 = Down(256,512)  # 原来是256, 512
+#         factor = 2 if self.bilinear else 1
+#         self.down4 = Down(512, 1024 // factor)  # 原来是512, 1024 // factor
+        
+#         # 移除self.ppm
+#         # self.ppm = PyramidPoolingModule(1024 // factor, [1, 2, 3, 6])  # 添加金字塔池化模块
+
+#         self.up1 = Up(1024, 512 // factor, self.bilinear)  # 原来是1024, 512 // factor
+#         self.up2 = Up(512, 256 // factor, self.bilinear)  # 原来是512, 256 // factor
+#         self.up3 = Up(256, 128 // factor, self.bilinear)  # 原来是256, 128 // factor
+#         self.up4 = Up(128, 64, self.bilinear)  # 原来是128, 64
+#         self.outc = OutConv(64, n_classes)  # 原来是64
+
+#     def forward(self, x):
+#         # 下采样部分
+#         x1 = self.inc(x)
+#         x2 = self.down1(x1)
+#         x3 = self.down2(x2)
+#         x4 = self.down3(x3)
+#         x5 = self.down4(x4)
+        
+#         # 移除了self.ppm(x5)
+        
+#         # 上采样部分
+#         x = self.up1(x5, x4)
+#         x = self.up2(x, x3)
+#         x = self.up3(x, x2)
+#         x = self.up4(x, x1)
+#         logits = self.outc(x)
+#         return logits
+
 class self_net(nn.Module):
     def __init__(self, n_channels=3, n_classes=4):
         super(self_net, self).__init__()
@@ -83,36 +115,52 @@ class self_net(nn.Module):
         self.n_classes = n_classes
         self.bilinear = False
 
-        self.inc = DoubleConv(n_channels, 64)  # 原来是64
-        self.down1 = Down(64, 128)  # 原来是64, 128
-        self.down2 = Down(128,256)  # 原来是128, 256
-        self.down3 = Down(256,512)  # 原来是256, 512
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
         factor = 2 if self.bilinear else 1
-        self.down4 = Down(512, 1024 // factor)  # 原来是512, 1024 // factor
+        self.down4 = Down(512, 1024 // factor)
         
-        # 移除self.ppm
-        # self.ppm = PyramidPoolingModule(1024 // factor, [1, 2, 3, 6])  # 添加金字塔池化模块
+        self.up1 = Up(1024, 512 // factor, self.bilinear)
+        self.up2 = Up(512, 256 // factor, self.bilinear)
+        self.up3 = Up(256, 128 // factor, self.bilinear)
+        self.up4 = Up(128, 64, self.bilinear)
+        self.outc = OutConv(64, n_classes)
 
-        self.up1 = Up(1024, 512 // factor, self.bilinear)  # 原来是1024, 512 // factor
-        self.up2 = Up(512, 256 // factor, self.bilinear)  # 原来是512, 256 // factor
-        self.up3 = Up(256, 128 // factor, self.bilinear)  # 原来是256, 128 // factor
-        self.up4 = Up(128, 64, self.bilinear)  # 原来是128, 64
-        self.outc = OutConv(64, n_classes)  # 原来是64
+        # Additional upsampling and concatenation layers for U-Net++
+        self.up1_2 = Up(512, 256 // factor, self.bilinear)
+        self.up2_2 = Up(256, 128 // factor, self.bilinear)
+        self.up3_2 = Up(128, 64, self.bilinear)
+
+        self.up1_3 = Up(256, 128 // factor, self.bilinear)
+        self.up2_3 = Up(128, 64, self.bilinear)
+
+        self.up1_4 = Up(128, 64, self.bilinear)
 
     def forward(self, x):
-        # 下采样部分
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-        
-        # 移除了self.ppm(x5)
-        
-        # 上采样部分
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
+
+        x_up1 = self.up1(x5, x4)
+        x_up2 = self.up2(x_up1, x3)
+        x_up3 = self.up3(x_up2, x2)
+        x_up4 = self.up4(x_up3, x1)
+
+        x_up1_2 = self.up1_2(x_up1, x3)
+        x_up2_2 = self.up2_2(x_up1_2, x2)
+        x_up3_2 = self.up3_2(x_up2_2, x1)
+
+        x_up1_3 = self.up1_3(x_up1_2, x2)
+        x_up2_3 = self.up2_3(x_up1_3, x1)
+
+        x_up1_4 = self.up1_4(x_up1_3, x1)
+
+        # Concatenate all outputs
+        x_out = torch.cat([x_up4, x_up3_2, x_up2_3, x_up1_4], dim=1)
+
+        logits = self.outc(x_out)
         return logits
