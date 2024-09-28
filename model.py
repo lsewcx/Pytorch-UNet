edc85c64a895f65c4bf8fe5309f6bfe0065f1c37
lsewcx <lse210085@163.com>
2024-09-28 21:57:54 +0800

    使用timm库

--- model.py
+++ model.py
@@ -79,17 +79,17 @@ class OutConv(nn.Module):
     
     
 class self_net(nn.Module):
-    def __init__(self, n_channels=3, out_channels=4):
+    def __init__(self, n_channels=3, n_classes=4):
         super().__init__()
         self.n_channels = n_channels
-        self.out_channels = out_channels
+        self.n_classes = n_classes
         self.encoder = timm.create_model('swin_tiny_patch4_window7_224', pretrained=True, in_chans=n_channels)
         self.decoder = nn.Sequential(
             Up(1024, 512, bilinear=True),
             Up(512, 256, bilinear=True),
             Up(256, 128, bilinear=True),
             Up(128, 64, bilinear=True),
-            OutConv(64, out_channels)
+            OutConv(64, n_classes)
         )
 
     def forward(self, x):
