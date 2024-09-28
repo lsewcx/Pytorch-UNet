add4df5518168eaff436b034b066da916bb70c89
lsewcx <lse210085@163.com>
2024-09-28 22:07:40 +0800

    使用timm库

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import timm
 
 class DepthwiseSeparableConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
@@ -76,64 +75,44 @@ class OutConv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
-    
-    
+
 class self_net(nn.Module):
     def __init__(self, n_channels=3, n_classes=4):
-        super().__init__()
+        super(self_net, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
-        self.encoder = timm.create_model('swin_tiny_patch4_window7_224', pretrained=True, in_chans=n_channels)
-        self.decoder = nn.Sequential(
-            Up(1024, 512, bilinear=True),
-            Up(512, 256, bilinear=True),
-            Up(256, 128, bilinear=True),
-            Up(128, 64, bilinear=True),
-            OutConv(64, n_classes)
-        )
+        self.bilinear = False
+
+        self.inc = DoubleConv(n_channels, 64)  # 原来是64
+        self.down1 = Down(64, 128)  # 原来是64, 128
+        self.down2 = Down(128,256)  # 原来是128, 256
+        self.down3 = Down(256,512)  # 原来是256, 512
+        factor = 2 if self.bilinear else 1
+        self.down4 = Down(512, 1024 // factor)  # 原来是512, 1024 // factor
+        
+        # 移除self.ppm
+        # self.ppm = PyramidPoolingModule(1024 // factor, [1, 2, 3, 6])  # 添加金字塔池化模块
+
+        self.up1 = Up(1024, 512 // factor, self.bilinear)  # 原来是1024, 512 // factor
+        self.up2 = Up(512, 256 // factor, self.bilinear)  # 原来是512, 256 // factor
+        self.up3 = Up(256, 128 // factor, self.bilinear)  # 原来是256, 128 // factor
+        self.up4 = Up(128, 64, self.bilinear)  # 原来是128, 64
+        self.outc = OutConv(64, n_classes)  # 原来是64
 
     def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
-
-# class self_net(nn.Module):
-#     def __init__(self, n_channels=3, n_classes=4):
-#         super(self_net, self).__init__()
-#         self.n_channels = n_channels
-#         self.n_classes = n_classes
-#         self.bilinear = False
-
-#         self.inc = DoubleConv(n_channels, 64)  # 原来是64
-#         self.down1 = Down(64, 128)  # 原来是64, 128
-#         self.down2 = Down(128,256)  # 原来是128, 256
-#         self.down3 = Down(256,512)  # 原来是256, 512
-#         factor = 2 if self.bilinear else 1
-#         self.down4 = Down(512, 1024 // factor)  # 原来是512, 1024 // factor
+        # 下采样部分
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
         
-#         # 移除self.ppm
-#         # self.ppm = PyramidPoolingModule(1024 // factor, [1, 2, 3, 6])  # 添加金字塔池化模块
-
-#         self.up1 = Up(1024, 512 // factor, self.bilinear)  # 原来是1024, 512 // factor
-#         self.up2 = Up(512, 256 // factor, self.bilinear)  # 原来是512, 256 // factor
-#         self.up3 = Up(256, 128 // factor, self.bilinear)  # 原来是256, 128 // factor
-#         self.up4 = Up(128, 64, self.bilinear)  # 原来是128, 64
-#         self.outc = OutConv(64, n_classes)  # 原来是64
-
-#     def forward(self, x):
-#         # 下采样部分
-#         x1 = self.inc(x)
-#         x2 = self.down1(x1)
-#         x3 = self.down2(x2)
-#         x4 = self.down3(x3)
-#         x5 = self.down4(x4)
+        # 移除了self.ppm(x5)
         
-#         # 移除了self.ppm(x5)
-        
-#         # 上采样部分
-#         x = self.up1(x5, x4)
-#         x = self.up2(x, x3)
-#         x = self.up3(x, x2)
-#         x = self.up4(x, x1)
-#         logits = self.outc(x)
-#         return logits
+        # 上采样部分
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
