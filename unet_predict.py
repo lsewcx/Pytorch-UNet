f4158804e1aa59124c641ec16a81ac24183a47fa
lsewcx <lse210085@163.com>
2024-09-29 23:46:12 +0800

    更改主干

--- unet_predict.py
+++ unet_predict.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
         try:
             import segmentation_models_pytorch as smp
             net = smp.Unet(
-            encoder_name="resnet18",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_name="mobilenet_v2",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
             encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
             in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
             classes=4,                      # model output channels (number of classes in your dataset)
