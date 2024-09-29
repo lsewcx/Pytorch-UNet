277eecb749926bdd33ff6ffa71665bdd7ef8688a
lsewcx <lse210085@163.com>
2024-09-29 10:51:13 +0800

    修改model代码

--- model.py
+++ model.py
@@ -110,11 +110,7 @@
 #         return logits
 
 
-
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
 class Conv2dReLU(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=False):
         super(Conv2dReLU, self).__init__()
@@ -126,6 +122,73 @@ class Conv2dReLU(nn.Module):
         x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
+        return x
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += identity
+        out = self.relu(out)
+        return out
+
+class ResNetEncoder(nn.Module):
+    def __init__(self):
+        super(ResNetEncoder, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = nn.Sequential(
+            BasicBlock(64, 64),
+            BasicBlock(64, 64)
+        )
+        self.layer2 = nn.Sequential(
+            BasicBlock(64, 128, stride=2, downsample=nn.Sequential(
+                nn.Conv2d(64, 128, kernel_size=1, stride=2, bias=False),
+                nn.BatchNorm2d(128)
+            )),
+            BasicBlock(128, 128)
+        )
+        self.layer3 = nn.Sequential(
+            BasicBlock(128, 256, stride=2, downsample=nn.Sequential(
+                nn.Conv2d(128, 256, kernel_size=1, stride=2, bias=False),
+                nn.BatchNorm2d(256)
+            )),
+            BasicBlock(256, 256)
+        )
+        self.layer4 = nn.Sequential(
+            BasicBlock(256, 512, stride=2, downsample=nn.Sequential(
+                nn.Conv2d(256, 512, kernel_size=1, stride=2, bias=False),
+                nn.BatchNorm2d(512)
+            )),
+            BasicBlock(512, 512)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
         return x
 
 class DecoderBlock(nn.Module):
@@ -174,55 +237,10 @@ class SegmentationHead(nn.Module):
         x = self.activation(x)
         return x
 
-class self_net(nn.Module):
+class UNet(nn.Module):
     def __init__(self):
-        super(self_net, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            nn.Sequential(
-                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(64),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(64)
-            ),
-            nn.Sequential(
-                nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(128),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(128),
-                nn.Sequential(
-                    nn.Conv2d(64, 128, kernel_size=1, stride=2, bias=False),
-                    nn.BatchNorm2d(128)
-                )
-            ),
-            nn.Sequential(
-                nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(256),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(256),
-                nn.Sequential(
-                    nn.Conv2d(128, 256, kernel_size=1, stride=2, bias=False),
-                    nn.BatchNorm2d(256)
-                )
-            ),
-            nn.Sequential(
-                nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(512),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(512),
-                nn.Sequential(
-                    nn.Conv2d(256, 512, kernel_size=1, stride=2, bias=False),
-                    nn.BatchNorm2d(512)
-                )
-            )
-        )
+        super(UNet, self).__init__()
+        self.encoder = ResNetEncoder()
         self.decoder = UnetDecoder()
         self.segmentation_head = SegmentationHead()
 
