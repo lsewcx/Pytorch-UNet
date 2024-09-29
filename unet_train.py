14e5088957ae3877ec6240dfd9f33c765430349a
lsewcx <lse210085@163.com>
2024-09-29 14:37:57 +0800

    修改基准模型深度

--- unet_train.py
+++ unet_train.py
@@ -232,8 +232,8 @@ if __name__ == '__main__':
             encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
             in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
             classes=4,                      # model output channels (number of classes in your dataset)
-            encoder_depth=6,
-            decoder_channels=(256, 128, 64, 32, 16,8),
+            encoder_depth=5,
+            decoder_channels=(512,256, 128, 64, 32),
             )
             total_params = sum(p.numel() for p in model.parameters())
             logging.info(f"模型的参数量: {total_params / 1e6:.2f}M")
