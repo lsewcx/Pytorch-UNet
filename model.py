379a2e64666164de3024fbc1608e3d584426127d
lsewcx <lse210085@163.com>
2024-09-29 11:00:37 +0800

    修改model代码

--- model.py
+++ model.py
@@ -111,6 +111,10 @@
 
 
 import torch.nn as nn
+from torchvision.models.resnet import ResNet
+from torchvision.models.resnet import BasicBlock
+from torchvision.models.resnet import Bottleneck
+
 class Conv2dReLU(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=False):
         super(Conv2dReLU, self).__init__()
@@ -147,49 +151,150 @@ class BasicBlock(nn.Module):
         out = self.relu(out)
         return out
 
-class ResNetEncoder(nn.Module):
-    def __init__(self):
-        super(ResNetEncoder, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = nn.Sequential(
-            BasicBlock(64, 64),
-            BasicBlock(64, 64)
+def patch_first_conv(model, new_in_channels, default_in_channels=3, pretrained=True):
+    """Change first convolution layer input channels.
+    In case:
+        in_channels == 1 or in_channels == 2 -> reuse original weights
+        in_channels > 3 -> make random kaiming normal initialization
+    """
+
+    # get first conv
+    for module in model.modules():
+        if isinstance(module, nn.Conv2d) and module.in_channels == default_in_channels:
+            break
+
+    weight = module.weight.detach()
+    module.in_channels = new_in_channels
+
+    if not pretrained:
+        module.weight = nn.parameter.Parameter(
+            torch.Tensor(
+                module.out_channels,
+                new_in_channels // module.groups,
+                *module.kernel_size,
+            )
         )
-        self.layer2 = nn.Sequential(
-            BasicBlock(64, 128, stride=2, downsample=nn.Sequential(
-                nn.Conv2d(64, 128, kernel_size=1, stride=2, bias=False),
-                nn.BatchNorm2d(128)
-            )),
-            BasicBlock(128, 128)
-        )
-        self.layer3 = nn.Sequential(
-            BasicBlock(128, 256, stride=2, downsample=nn.Sequential(
-                nn.Conv2d(128, 256, kernel_size=1, stride=2, bias=False),
-                nn.BatchNorm2d(256)
-            )),
-            BasicBlock(256, 256)
-        )
-        self.layer4 = nn.Sequential(
-            BasicBlock(256, 512, stride=2, downsample=nn.Sequential(
-                nn.Conv2d(256, 512, kernel_size=1, stride=2, bias=False),
-                nn.BatchNorm2d(512)
-            )),
-            BasicBlock(512, 512)
+        module.reset_parameters()
+
+    elif new_in_channels == 1:
+        new_weight = weight.sum(1, keepdim=True)
+        module.weight = nn.parameter.Parameter(new_weight)
+
+    else:
+        new_weight = torch.Tensor(
+            module.out_channels, new_in_channels // module.groups, *module.kernel_size
         )
 
+        for i in range(new_in_channels):
+            new_weight[:, i] = weight[:, i % default_in_channels]
+
+        new_weight = new_weight * (default_in_channels / new_in_channels)
+        module.weight = nn.parameter.Parameter(new_weight)
+
+
+def replace_strides_with_dilation(module, dilation_rate):
+    """Patch Conv2d modules replacing strides with dilation"""
+    for mod in module.modules():
+        if isinstance(mod, nn.Conv2d):
+            mod.stride = (1, 1)
+            mod.dilation = (dilation_rate, dilation_rate)
+            kh, kw = mod.kernel_size
+            mod.padding = ((kh // 2) * dilation_rate, (kh // 2) * dilation_rate)
+
+            # Kostyl for EfficientNet
+            if hasattr(mod, "static_padding"):
+                mod.static_padding = nn.Identity()
+
+class EncoderMixin:
+    """Add encoder functionality such as:
+    - output channels specification of feature tensors (produced by encoder)
+    - patching first convolution for arbitrary input channels
+    """
+
+    _output_stride = 32
+
+    @property
+    def out_channels(self):
+        """Return channels dimensions for each tensor of forward output of encoder"""
+        return self._out_channels[: self._depth + 1]
+
+    @property
+    def output_stride(self):
+        return min(self._output_stride, 2**self._depth)
+
+    def set_in_channels(self, in_channels, pretrained=True):
+        """Change first convolution channels"""
+        if in_channels == 3:
+            return
+
+        self._in_channels = in_channels
+        if self._out_channels[0] == 3:
+            self._out_channels = tuple([in_channels] + list(self._out_channels)[1:])
+
+        patch_first_conv(
+            model=self, new_in_channels=in_channels, pretrained=pretrained
+        )
+
+    def get_stages(self):
+        """Override it in your implementation"""
+        raise NotImplementedError
+
+    def make_dilated(self, output_stride):
+        if output_stride == 16:
+            stage_list = [5]
+            dilation_list = [2]
+
+        elif output_stride == 8:
+            stage_list = [4, 5]
+            dilation_list = [2, 4]
+
+        else:
+            raise ValueError(
+                "Output stride should be 16 or 8, got {}.".format(output_stride)
+            )
+
+        self._output_stride = output_stride
+
+        stages = self.get_stages()
+        for stage_indx, dilation_rate in zip(stage_list, dilation_list):
+            replace_strides_with_dilation(
+                module=stages[stage_indx], dilation_rate=dilation_rate
+            )
+            
+class ResNetEncoder(ResNet, EncoderMixin):
+    def __init__(self, out_channels, depth=5, **kwargs):
+        super().__init__(**kwargs)
+        self._depth = depth
+        self._out_channels = out_channels
+        self._in_channels = 3
+
+        del self.fc
+        del self.avgpool
+
+    def get_stages(self):
+        return [
+            nn.Identity(),
+            nn.Sequential(self.conv1, self.bn1, self.relu),
+            nn.Sequential(self.maxpool, self.layer1),
+            self.layer2,
+            self.layer3,
+            self.layer4,
+        ]
+
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        return x
+        stages = self.get_stages()
+
+        features = []
+        for i in range(self._depth + 1):
+            x = stages[i](x)
+            features.append(x)
+
+        return features
+
+    def load_state_dict(self, state_dict, **kwargs):
+        state_dict.pop("fc.bias", None)
+        state_dict.pop("fc.weight", None)
+        super().load_state_dict(state_dict, **kwargs)
 
 class DecoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
