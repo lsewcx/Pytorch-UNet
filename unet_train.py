8667756f8116b2608f883b589858949de4f972cf
lsewcx <lse210085@163.com>
2024-09-30 14:36:58 +0800

    去掉预训练权重

--- unet_train.py
+++ unet_train.py
@@ -228,9 +228,9 @@ if __name__ == '__main__':
     elif args.model == 'UNet':
         try:
             import segmentation_models_pytorch as smp
-            model = smp.UnetPlusPlus(
+            model = smp.Unet(
             encoder_name="resnet50",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-            encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+            encoder_weights=None,     # use `imagenet` pre-trained weights for encoder initialization
             in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
             classes=4,                      # model output channels (number of classes in your dataset)
             )
