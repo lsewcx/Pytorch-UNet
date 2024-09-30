b2e15f5169d929308d4b0431cc218479bc19469d
lsewcx <lse210085@163.com>
2024-09-30 13:39:19 +0800

    修改jupyter

--- unet_train.py
+++ unet_train.py
@@ -229,7 +229,7 @@ if __name__ == '__main__':
         try:
             import segmentation_models_pytorch as smp
             model = smp.Unet(
-            encoder_name="resnet18",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_name="resnet50",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
             encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
             in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
             classes=4,                      # model output channels (number of classes in your dataset)
