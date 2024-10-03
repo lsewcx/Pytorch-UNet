284368ab597d2f3184a5b983b01f1c6651566eba
lsewcx <lse210085@163.com>
2024-10-03 18:01:32 +0800

    优化代码

--- model.py
+++ model.py
@@ -214,16 +214,18 @@ class self_net(nn.Module):
         self.bn1 = nn.BatchNorm2d(64)
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(128)
-        self.conv3 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.deconv = nn.ConvTranspose2d(128, 4, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.deconv1 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.deconv2 = nn.ConvTranspose2d(128, 4, kernel_size=3, stride=2, padding=1, output_padding=1)
 
     def forward(self, x):
-        x = torch.relu(self.bn1(self.conv1(x)))
-        x = torch.relu(self.bn2(self.conv2(x)))
-        x = torch.relu(self.bn3(self.conv3(x)))
-        x = self.deconv(x)
-        return x
+        x1 = torch.relu(self.bn1(self.conv1(x)))
+        x2 = torch.relu(self.bn2(self.conv2(x1)))
+        x3 = torch.relu(self.bn3(self.conv3(x2)))
+        x4 = self.deconv1(x3)
+        x5 = self.deconv2(x4 + x2)
+        return x5
 
 
 
