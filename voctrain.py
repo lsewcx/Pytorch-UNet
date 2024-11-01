c2eb3d629e7ea1318139b23294f468c9fc122365
lsewcx <lse210085@163.com>
2024-11-01 15:19:03 +0800

    修复

--- voctrain.py
+++ voctrain.py
@@ -334,7 +334,6 @@ if __name__ == "__main__":
             learning_rate=args.lr,
             device=device,
             img_scale=args.scale,
-            val_percent=args.val / 100,
             amp=args.amp,
         )
     except torch.cuda.OutOfMemoryError:
@@ -352,6 +351,5 @@ if __name__ == "__main__":
             learning_rate=args.lr,
             device=device,
             img_scale=args.scale,
-            val_percent=args.val / 100,
             amp=args.amp,
         )
